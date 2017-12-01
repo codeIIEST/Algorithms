ce0f4bdc8bc0c88581e7d51aee8690b4c1115176
ADIL MAHMOOD <adilm5.malda@gmail.com>
2017-12-01 15:45:59 +0530

    updated level_order_traversal

--- BInary_Tree/level_order_traversal.c
+++ BInary_Tree/level_order_traversal.c
@@ -1,6 +1,10 @@
+/*C programme for Level Order Traversal of a Binary Search Tree
+  Approach- Recursive*/
+
 #include<stdio.h>
 #include<stdlib.h>
-////////////////////////////////////////////////////
+/*Creating the structure of binary tree node having data amd pointers to left 
+  and right child*/ 
 typedef struct Node
 {
 	int data;
@@ -8,12 +12,12 @@ typedef struct Node
 	struct Node* right;
 
 }T;
-///////////////////////////////////////////////////
+//Function Prototypes
 T* newnode(int);
 void LevelOrder(T*);
 T* printGivenLevel(T*,int);
 int height(T*);
-///////////////////////////////////////////////////
+
 int main()
 {
 	T* root=newnode(1);//Creating the tree
@@ -25,7 +29,7 @@ int main()
 	LevelOrder(root);
 
 }
-
+//Function to allocates a new node, and initiales the left and right child with NULL
 T* newnode(int value)
 {
  	T* temp=(T*)malloc(sizeof(T));
@@ -35,14 +39,15 @@ T* newnode(int value)
  	return temp;
 
 }
-
+//Function that drives the level order traversal of the tree
 void LevelOrder(T* root)
 {
 	int level;
 	for(level=1;level<=height(root);level++)
 		{printGivenLevel(root,level);}
 }
-
+/*Computes the height of the tree, which is the length of path from the root to
+  to the deepest leaf node*/
 int height(T* root)
 {
 	if(root==NULL)
@@ -53,6 +58,7 @@ int height(T* root)
 	return lh>rh?lh+1:rh+1;
 }
 
+//Prints the nodes in each level of the tree
 T* printGivenLevel(T* root,int level)
 {
 	if(root==NULL)return NULL;
