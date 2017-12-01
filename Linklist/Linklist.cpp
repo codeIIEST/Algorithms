0dc04b6a570e984ea65b6c47ccb0b19c95b16a0e
sgrG24 <sagargupta2405@gmail.com>
2017-12-01 19:52:31 +0530

    Add comments in each section

--- Linklist/Linklist.cpp
+++ Linklist/Linklist.cpp
@@ -29,17 +29,24 @@ class linklist{
 //Default constructor
 linklist ::  linklist()
 {
+	//Initialize start with NULL
 	start= NULL;
 }
 
 //Add element at last of the list
 void linklist::append(int x)
 {
+	//Creating the new node
 	node* temp = new node;
+	
+	//assigning the data in the node                                   
 	temp->d=x;
+	
+	//putting next of the node to NULL                                               
 	temp->next= NULL;
 	
-	if(start== NULL)
+	//If list is empty
+	if(start== NULL)                                         
 	{
 		start= temp;
 	}
@@ -48,89 +55,121 @@ void linklist::append(int x)
 		node* t;
 		t= start;
 		
+		//this will find the pointer to last node of the list
 		while(t->next!= NULL)
 		{
 			t=t->next;
 		}
 		
+		//update the last node's next pointer with the pointer to the new node
 		t->next= temp;
 	}
 }
 
 //Add element at the begining of the list
 void linklist::add_beg(int x)
-{
+{	
+	//creating new node
 	node* temp= new node;
+	
+	//assigning the data in the node
 	temp->d= x;
+	
+	//putting nexy of the node to the NULL
 	temp->next= NULL;
 	
+	//If list is empty
 	if(start==NULL)
 	{
 		start= temp;
 	}
 	else
 	{
+		//update the next of current node with start
 		temp->next= start;
+		
+		//updating start
 		start= temp;
 	}
+}
+
+//returns the number of nodes in the linklist.
+int linklist::count()
+{
+	int count=0;
+	
+	//create temporary node
+	//assigned it with start 
+	node* temp= start;
+	
+	//traverse the whole list
+	while(temp!=NULL)
+	{
+		//updating count
+		count++;
+		//updating temp
+		temp=temp->next;
+	}
+	return count;
 }
 
 //Add element at the middle of the list
 void linklist::add_middle(int x)
 {
-	int count=0;
+	//total number of nodes in the list
+	int len=count();
+	
 	int n;
 	node* t = start;
+	
+	//creating new node and assigning the values
 	node* temp= new node;
 	temp->d=x;
 	temp->next=NULL;
-	while(t!=NULL)
-	{
-		count++;
-		t=t->next;
-	}
-	t=start;
-	if(count%2==0)
-	n=count/2;
+	
+	//if list has even numbers of node, then add it at len/2
+	if(len%2==0)
+	n=len/2;
 	else
-	n=((count/2)+1);
+	n=((len/2)+1);
+	
+	//if the list is empty
 	if(n==0)
 	{
 		start=temp;
 		return;
 	}
+	
+	//traversing to the n'th node
 	for(int i=1;i<n;i++)
 	{
 		t=t->next;
 	}
+	//updating temp
 	temp->next=t->next;
 	t->next=temp;
 	return;
 }
 
 //Add element after specific position
-void linklist::add_after(int n, int x)
+void linklist::add_after(int pos, int x)
 {
+	//creating new node and assigning values in it
 	node* temp= new node;
 	temp->d=x;
 	temp->next=NULL;
 	
 	node* t = start;
-	int count=0;
-	while(t!= NULL)
-	{
-		count++;
-		t=t->next;
-	}
-	t=start;
+	int len=count();
 	
-	if(n>count)
+	//if entered position is greater then number of node present in list
+	if(pos>len)
 	{
 		cout<<"Invalid Position To Add Data"<<endl;
 	}
 	else
 	{
-		for(int i=1;i<n;i++)
+		for(int i=1;i<pos;i++)
 		{
 			t=t->next;
 		}
@@ -156,32 +195,43 @@ void linklist::display()
 //Delete the middle element from the list
 void linklist::del_middle()
 {
-	int n, coun=0;
+	int n, len=0;
 	node* t= start;
-	coun=count();
-	if(coun%2==0)
-	n= coun/2;
+	
+	//counting total number of nodes
+	len=count();
+	
+	//if even numbers of nodes are there in the list
+	if(len%2==0)
+	n= len/2;
 	else
-	n=(coun/2)+1;
+	n=(len/2)+1;
+	
+	//if list is empty
 	if(n==0)
 	{
 		cout<<"List is empty";
 		return;
 	}
-	if(n==1&& coun==1)
+	
+	//if there is only 1 element i.e. only start node is there
+	if(n==1&& len==1)
 	{
 		delete start;
 		start=NULL;
 		return;
 	}
-	if(n==1&& coun==2)
+	
+	//if 2 nodes are present in the list
+	if(n==1&& len==2)
 	{
 		delete start;
 		start=t->next;
 		return;
-	}
-	else
+	}	
+	else														//more than two nodes are there in the list
 	{
+		//traversed to middle node
 		for(int i=1;i<n-1;i++)
 		{
 			t=t->next;
@@ -192,25 +242,17 @@ void linklist::del_middle()
 	}
  } 
 
-//Count the elements in the linklist.
-int linklist::count()
-{
-	int count=0;
-	node* temp= start;
-	while(temp!=NULL)
-	{
-		count++;
-		temp=temp->next;
-	}
-	return count;
-}
+
 
 //Delete a specific element from the list.
 void linklist::del(int x)
-{
+{	
 	int flag=0;
+	//creating temporary node
 	node* temp = start;
 	node* t1;
+	
+	//if start node is that node
 	if(start->d== x)
 	{
 		t1=start->next;
@@ -219,6 +261,7 @@ void linklist::del(int x)
 	}
 	else
 	{
+		//traversing to that node
 		while(temp->next!=NULL)
 		{
 			if(temp->next->d== x)
@@ -232,6 +275,7 @@ void linklist::del(int x)
 			
 			temp= temp->next;
 		}
+		//if the value is not present in the list
 		if(flag==0)
 		cout<<"Error: Element Not Found"<<endl;
 	}
@@ -241,18 +285,19 @@ void linklist::del(int x)
 void linklist::sort()
 {
 	node* temp =start;
-	int n= count();
-	node* cnode;
+	int len= count();
+	node* current_node;
 	node* t;
-	while(n--)
-	{
-		cnode= temp;
+	//sorting whole list
+	while(len--)
+	{	
+		current_node= temp;
 		t = temp;
 		while(t!=NULL)
 		{
-			if(cnode->d>t->d)
+			if(current_node->d>t->d)
 			{
-				swap(cnode->d,t->d);
+				swap(current_node->d,t->d);
 			}
 			t=t->next;
 		}
@@ -260,11 +305,14 @@ void linklist::sort()
 	}
 }
 
-//Merge two sorted linklist.
+//takes two sorted list in increasing order and merge them.
 void linklist::merge(linklist h, linklist k)
 {
+	//creating two node pointer and initialized with it by start pointers
 	node* t1 = h.start;
 	node* t2 = k.start;
+	
+	//traversing till the end of any list
 	while(t1!= NULL && t2!= NULL)
 	{
 	if(t1->d < t2->d)
@@ -310,6 +358,8 @@ void linklist::merge(linklist h, linklist k)
 		t2= t2->next;
 	}
 	}
+	
+	//merge remaining nodes of list 1 if any
 	while(t1!=NULL)
 	{
 		node* temp = new node;
@@ -324,7 +374,8 @@ void linklist::merge(linklist h, linklist k)
 		t->next= temp;
 		t1= t1->next;
 	}
-			
+	
+	//merge remaining nodes of list 2 if any		
 	while(t2!=NULL)
 	{
 		node* temp = new node;
@@ -345,14 +396,16 @@ void linklist::merge(linklist h, linklist k)
 //Reverse the elements of the linklist
 linklist linklist::reverse()
 {
+	//reverse the linklist
 	node* t=start;
 	linklist temp;
 	for(int i=1;i<=count();i++)
 	{
+		//adding the elements from the last to the new linklist
 	    temp.add_beg(t->d);
 	    t=t->next;
 	}
-	return linklist;
+	return temp;
 }
 
 //Main function
@@ -360,16 +413,16 @@ int main()
 {
 	linklist l;
 	l.append(5);
-	l.append(5455);
-	l.append(645);
-	l.append(545);
+	l.append(54);
+	l.append(64);
+	l.append(55);
 	l.add_beg(56);
-	l.add_beg(898);
+	l.add_beg(88);
 	l.add_after(5, 23);
-	l.add_after(2, 56);
-	l.del(898);
-	l.del(545);
-	l.add_middle(45);
+	l.add_after(2, 6);
+	l.del(98);
+	l.del(45);
+	l.add_middle(5);
 	l.del_middle();
 	
 	l.display();
