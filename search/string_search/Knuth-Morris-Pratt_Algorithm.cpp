43832d232973305a13fbacd88f87cb43ec31102c
nandikeshsingh <nandikeshsingh@gmail.com>
2017-12-02 16:40:17 +0530

    Update Knuth-Morris-Pratt_Algorithm.cpp

--- search/string_search/Knuth-Morris-Pratt_Algorithm.cpp
+++ search/string_search/Knuth-Morris-Pratt_Algorithm.cpp
@@ -8,27 +8,29 @@ int main()
 	string text;
 	string pattern;
 
-	cin>>text;
+	cin>>text;//inputting text	
 	cout<<endl;
 
-	cin>>pattern;
+	cin>>pattern;//inputting pattern
 	cout<<endl;
 
 	int pattern_length=pattern.size();//variable to store length of pattern string
 	int text_length=text.size();//variable to store length of text string     
 
-	if(pattern_length>text_length)
+	if(pattern_length>text_length)//if pattern_length is greater than text it is not possible to find pattern in text.
 	{
 		cout<<"Not Found"<<endl;
 		exit(0);
 	}
 
-	int temp[pattern_length];//i th element in the temp array will store the length of the longest suffix which is equal to the prefix in pattern array upto i th position
+	int temp[pattern_length];//i th element in the temp array will store the length of the longest suffix which is equal to the prefix in pattern array upto i th index.
 
 	int i=1,j=0;
 
+	/*Suppose the patteren is ababa then temp[4] would store value 3 as the length of longest prefix(aba) which is equal 
+	to the longest suffix(aba) is 3 upto 4 th index in the pattern.The entire temp array would look like 0 0 1 2 3.*/
 	temp[0]=0;
-	while(i<pattern_length)
+	while(i<pattern_length)//Building the temp array.
 	{
 		if(pattern[i]==pattern[j])
 		{
@@ -49,7 +51,9 @@ int main()
 
 	i=0;
 	j=0;
-
+/*Suppose our text is abacd and our pattern is abad.During our first mismatch that is at d(j=3)in pattern and c(i=3)in text,
+unlike the naive string search we won't start again from i=1 in text and j=0 in pattern instead we look in temp[2] which will
+have value 1 and we set j to 1 and we will again start doing search from i=3 and j=1.*/
 	while(j<pattern_length && i<text_length)  //implementing the KMP alogorithm
 	{
 		if(pattern[j]==text[i])
