898cab7269e6324a58b782b98a49debe0f27a19c
rahuldcoder <rahulthakur02k@gmail.com>
2017-12-02 16:55:22 +0530

    added python implementation of bubble sort algorithm

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,25 +1,34 @@
 #implementation of bubble sort algorithm
+# Author: Rahul Thakur
+# Date:02 December 2017
 
 def bubbleSort(l1):
+
     size=len(l1)
     i=0
+
     while i<size :
         j=0
+
         while j< size-i-1:
             if(l1[j]>l1[j+1]):
                 temp=l1[j]
                 l1[j]=l1[j+1]
                 l1[j+1]=temp
             j=j+1
-        #    print(l1)
+
         i=i+1
-    #print(l1)
+
     return l1
+
 print("Enter the size of the list")
+
 x=int(input())
+
 print("Enter the elements of the list")
 
 list1=list(map(int,input().split()))
 
 print("Array after bubble sort is")
+
 print(bubbleSort(list1))
