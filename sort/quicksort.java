dc41a5a7dbd663eff97d37af9a9a51ed4b889185
Awani-Shukla <31660543+Awani-Shukla@users.noreply.github.com>
2017-12-02 19:31:40 +0530

    quicksort.java
    this is solution to the code of quicksort program in java language.
    the above code has complexity :-
    *The complexity of quick sort in the average case is Θ(n log(n)) and in the worst case is Θ(n2).

---
+++ sort/quicksort.java
@@ -0,0 +1,58 @@
+public class MyQuickSort {
+     
+    private int array[];
+    private int length;
+ 
+    public void sort(int[] inputArr) {
+         
+        if (inputArr == null || inputArr.length == 0) {
+            return;
+        }
+        this.array = inputArr;
+        length = inputArr.length;
+        quickSort(0, length - 1);
+    }
+ 
+    private void quickSort(int lowerIndex, int higherIndex) {
+         
+        int i = lowerIndex;
+        int j = higherIndex;
+        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+              while (i <= j) {
+           
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                exchangeNumbers(i, j);
+               
+                i++;
+                j--;
+            }
+        }
+        
+        if (lowerIndex < j)
+            quickSort(lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(i, higherIndex);
+    }
+ 
+    private void exchangeNumbers(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+     
+    public static void main(String a[]){
+         
+        MyQuickSort sorter = new MyQuickSort();
+        int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+        sorter.sort(input);
+        for(int i:input){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
