770238ad5f9a60a9265c1710c700c71777654ada
rahulkumaran <rahuldravid313@gmail.com>
2017-12-02 14:09:48 +0530

    Update caesarcipher1.py

--- Cryptography/caesarcipher1.py
+++ Cryptography/caesarcipher1.py
@@ -1,16 +1,12 @@
 """
-A general purpose function for both, encryption and decryption.
-After giving the string that you want, if you decide to shift it to right,
-encryption will take place by shifting characters to the right and
+A caesar cipher function for both, encryption and decryption.
+After giving the string that you want, encryption will take place by shifting characters to the right and
 the decrypted string can be obtained by shifting the encrypted string to the left.
-The general purpose function takes care of both the left and the right shifts.
-Based on how the user uses it, he or she can encrypt and decrypt the data.
 """
 
 
 def encrypt(message,key): 
   ascii_vals=[ord(c) for c in message]                #To get a list of ASCII Values of each character in string
-  for i in range(0,len(ascii_vals)):
     ascii_vals[i] = ascii_vals[i] + key
     if(ascii_vals[i]>97 and ascii_vals[i]>122):  #If the values in the list are greater than 97 and 122, we get it back b/w 97 and 122
       ascii_vals[i] = 97 + (ascii_vals[i]-122) -1
