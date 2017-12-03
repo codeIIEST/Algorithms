0ac436cc7a84f676b9bbc065be27daee9364a9b8
Shivesh Choudhary <32201319+shiveshchoudhary@users.noreply.github.com>
2017-12-03 21:17:18 +0530

    py version of bubble sort

---
+++ sort/bubblesort.py
@@ -0,0 +1,19 @@
+def bubbleSort(l1):
+    size=len(l1)
+    i=0
+    while i<size :
+        j=0
+        while j< size-i-1:
+            if(l1[j]>l1[j+1]):
+                temp=l1[j]
+                l1[j]=l1[j+1]
+                l1[j+1]=temp
+                j=j+1
+         i=i+1
+     return l1
+print("Enter the size of the list")
+x=int(input())
+print("Enter the elements of the list")
+list1=list(map(int,input().split()))
+print("Array after bubble sort is")
+ print(bubbleSort(list1))
