afe2107352bbea8ff8a63354ecfe80bc6292b6fe
Shivesh Choudhary <32201319+shiveshchoudhary@users.noreply.github.com>
2017-12-03 21:25:25 +0530

    Update bubblesort.py

--- sort/bubblesort.py
+++ sort/bubblesort.py
@@ -9,11 +9,11 @@ def bubbleSort(l1):
                 l1[j]=l1[j+1]
                 l1[j+1]=temp
                 j=j+1
-         i=i+1
+        i=i+1
      return l1
 print("Enter the size of the list")
 x=int(input())
 print("Enter the elements of the list")
 list1=list(map(int,input().split()))
 print("Array after bubble sort is")
- print(bubbleSort(list1))
+print(bubbleSort(list1))
