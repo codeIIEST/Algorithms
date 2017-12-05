608bc8953d5c1d11f2e60eaca593b0033dffec52
Eshan <23567081+AdvikEshan@users.noreply.github.com>
2017-12-05 22:56:47 +0530

    Update HeapSort.cpp

--- sort/HeapSort/HeapSort.cpp
+++ sort/HeapSort/HeapSort.cpp
@@ -1,49 +1,47 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
+#define rep(i,a,b,c) for(i=a;i<b;i+=c)
+#define repp(i,a,b,c) for(i=a;i>b;i+=c)
 
 //--------------------------------------Heapify 
 void max_heapify(int *a, int i, int n)
 {
-    int left, largest;
-    largest = a[i];
-    left = 2*i;
-    while (left <= n)
-    {
-        if (left < n && a[left+1] > a[left])
-            left = left+1;
-        if (largest > a[left])
-            break;
-        else if (largest <= a[left])
+    int l=2*i+1;
+     int r=2*i+2;
+     int largest=0;
+     if(l<=n and a[l]>a[i])
+     	 largest=l;
+     else
+         largest=i;
+     if(r<=n and a[r]>a[largest])
+         largest=r;
+     if(largest!=i)
         {
-            a[left/2] = a[left];
-            left = 2*left;
+        	swap(a[largest],a[i]);
+        	max_heapify(a,largest,n);
         }
+}
+//---------------------------------Build max Heap
+void build_maxheap(int *a, int n)
+{
+	int i;
+     repp(i,n/2,-1,-1)
+    {
+    	max_heapify(a,i,n);
     }
-    a[left/2] = largest;
-    return;
 }
 
 //-----------------------------------HeapSort
 void heapsort(int *a, int n)
 {
-    int i, largest;
-    for (i = n; i >= 2; i--)
-    {
-        largest = a[i];
-        a[i] = a[1];
-        a[1] = largest;
-        max_heapify(a, 1, i - 1);
-    }
-}
-
-//---------------------------------Build max Heap
-void build_maxheap(int *a, int n)
-{
-    int i;
-    for(i = n/2; i >= 1; i--)
-    {
-        max_heapify(a, i, n);
-    }
+	 int i;
+    build_maxheap(a,n);
+    repp(i,n,-1,-1)
+       {
+       	   swap(a[0],a[i]);
+       	   n--;
+       	   max_heapify(a,0,n);
+       }
 }
 
 int main()
@@ -51,18 +49,21 @@ int main()
     int n, i, x;
     cout<<"Enter no of elements of array\n";
     cin>>n;
-    int a[20];
-    for (i = 1; i <= n; i++)
-    {
-        cout<<"Enter element "<<(i)<<endl;
-        cin>>a[i];
-    }
-    build_maxheap(a,n);
-    heapsort(a, n);
-    cout<<"\n\nSorted Array :\n";
-    for (i = 1; i <= n; i++)
-    {
-        cout<<a[i]<<endl;
-    }
+    
+    //Dynamic Memory Allocation
+    int *dynamic;
+    dynamic = new int[n];
+	
+	//Input Of Elements
+	cout<<"Enter the elements of array :\n";
+    rep(i,0,n,1)
+       cin>>dynamic[i];
+    
+    heapsort(dynamic,n-1);  
+    //Sorted Array using heap sort
+    cout<< "Sorted Sequence is : " ;
+	
+	rep(i,0,n,1)
+      cout<< dynamic[i]<<"\t";
     return 0;
 }
