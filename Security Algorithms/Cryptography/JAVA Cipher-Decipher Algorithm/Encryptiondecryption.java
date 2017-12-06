a85f1fdfc9939b13052720401b7dbe2ed1375d8c
Sagar <32388717+sagar9268@users.noreply.github.com>
2017-12-07 01:49:49 +0530

    Cipher-Decipher algorithm added along with readme

---
+++ Security Algorithms/Cryptography/JAVA Cipher-Decipher Algorithm/Encryptiondecryption.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+package cryptography;
+public class Encryptiondecryption {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the encryption key(1-25):");
+		int e=s.nextInt();
+		String ptext;
+		String ctext="";
+		char ch;
+		System.out.println("Plaintext-Uppercase Letters Only:");
+		ptext=s.next();
+		int len=ptext.length();
+		//Encryption starts from here.
+		for(int i=0;i<len;i++)
+		{
+			ch=ptext.charAt(i);
+			ch=(char)(ch+e);
+			if(ch>90)
+				ch=(char)(ch-26);
+			ctext=ctext+ch;
+			
+		}
+		System.out.println("Ciphertext:"+ctext);
+		//Decryption starts from here.
+		System.out.println("Enter Ciphertext-Uppercase Letters Only:");
+		ctext=s.next();
+		len=ctext.length();
+		System.out.println("Plaintext for each encryption key from 1 to 25:");
+		ptext="";
+		for(int i=1;i<=25;i++)
+		{
+			for(int j=0;j<len;j++)
+			{
+				ch=ctext.charAt(j);
+				ch=(char)(ch-i);
+				if(ch<65)
+					ch=(char)(ch+26);
+				ptext=ptext+ch;
+			}
+			System.out.println("Key "+i+" : Plaintext="+ptext);
+			ptext="";
+		}
+		
+
+	}
+
+}
