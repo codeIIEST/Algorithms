2f22f09fc57decf6b2268f4dba82fd515aa07d9d
Vinay Varma <vinay.n.varma189@gmail.com>
2017-12-07 17:41:14 +0530

    fixed some issues

--- Competitive Coding/Graphs/unionfind/CycleDetection.java
+++ Competitive Coding/Graphs/unionfind/CycleDetection.java
@@ -44,17 +44,6 @@ class Unionfind
     }
     public static void main(String args[])
     {
-        Graph g1 = new Graph(5);
-        g1.addEdge(1, 0);
-        g1.addEdge(0, 2);
-        g1.addEdge(2, 0);
-        g1.addEdge(0, 3);
-        g1.addEdge(3, 4);
-        if (g1.isCyclic())
-            System.out.println("Graph contains cycle");
-        else
-            System.out.println("Graph doesn't contains cycle");
-
         Graph g2 = new Graph(3);
         g2.addEdge(0, 1);
         g2.addEdge(1, 2);
