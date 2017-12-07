38ad170a4fdf469cf9d89940e46c141f1ea78134
Vinay Varma <vinay.n.varma189@gmail.com>
2017-12-07 17:27:28 +0530

    fixed issues

--- Competitive Coding/Graphs/unionfind/CycleDetection.java
+++ Competitive Coding/Graphs/unionfind/CycleDetection.java
@@ -38,8 +38,7 @@ class Unionfind
         for (int i = 0; i < V; i++)
             visited[i] = false;
         for (int u = 0; u < V; u++)
-            if (!visited[u])
-                if (isCyclicUtil(u, visited, -1))
+             if (!visited[u] && isCyclicUtil(u, visited, -1))
                     return true;
         return false;
     }
