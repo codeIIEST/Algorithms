2e1fb41dcbca5cdb42a790499d3b66857db007ce
nandikeshsingh <nandikeshsingh@gmail.com>
2017-12-09 20:33:37 +0530

    Rod Cutting Problem

---
+++ Competitive Coding/Dynamic Programming/Rod Cutting/Rod Cutting.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int rod_length;
+	cout<<"Enter the length of the rod : ";
+	cin>>rod_length;
+	cout<<endl;
+
+	int price[rod_length];//price[i] stores the price of rod of length i+1.
+	for(int i=0;i<rod_length;i++)
+	{
+			cout<<"Enter the price of rod of length "<<i+1<<" : ";
+ 			cin>>price[i];
+	}
+	cout<<endl;
+	
+ 	int temp[rod_length+1][rod_length+1];
+
+ 	for(int i=0;i<rod_length+1;i++)
+ 	{
+ 		for(int j=0;j<rod_length+1;j++)
+ 		{
+ 			if(j==0)
+ 				temp[i][j]=0;
+ 			else if(i==0)
+ 				temp[i][j]=price[i-1];
+ 			else if(i>j)
+ 				temp[i][j]=temp[i-1][j];
+ 			else
+ 				temp[i][j]=max(price[i-1]+temp[i][j-i],temp[i-1][j]);
+ 		}
+ 	}
+
+ 	cout<<"The Maximum value that can be obtained is : "<<temp[rod_length][rod_length]<<endl;
+
+ 	return 0;
+}
---
+++ Competitive Coding/Dynamic Programming/Rod Cutting/Rod Cutting_Readme.md
@@ -0,0 +1,22 @@
+Rod-Cutting Problem :-
+
+Algorithm Paradigm : Dynamic Programming
+
+Time Complexity : Quadratic
+
+Space Complexity : Quadratic
+
+Problem-statement:
+Given a rod of length n and an array of prices of rods of length less than or equal to n.Determine the maximum value that can be obtained by cutting the rod and selling the pieces.
+
+Working:
+
+Here price array stores the price of rod of different lengths.eg:price[3] stores the price of rod of length 4. 
+
+Using the temp array we are building the solution using the best value of smaller cases.
+temp[i][j] stores the maximum price  that can be obtained if we had rod of length j and at max i cuts were allowed.
+
+Using his technique we are bringing the time complexity down from exponential (as in case of brute force solution) to polynomial.
+
+
+
