6eb31b2e5e9b79490d870c5f7d36510642835dc7
Rahul Arulkumaran <rahuldravid313@gmail.com>
2017-12-14 13:42:11 +0530

    Update and rename proj_euler_21.py to sum_of_divisors.py

--- Competitive Coding/Math/Project_Euler/sum_of_divisors.py
+++ Competitive Coding/Math/Project_Euler/sum_of_divisors.py
@@ -1,9 +1,9 @@
 def divisors(n): #function to find divisors of a number
-	sum=1	#initialising sum as 1 as 1 is a multiple of all numbers
+	sum_digits=1	#initialising sum as 1 as 1 is a multiple of all numbers
 	for i in range(2,n/2+1):	#running loop from 2 to n/2+1 as the highest possible divisor of a number is half of the num
 		if(n%i==0):
-			sum+=i		#calculating sum of divisors
-	print sum		#prints sum of diviors
+			sum_digits+=i		#calculating sum of divisors
+	print sum_digits		#prints sum of diviors
 
 
 if __name__=='__main__':
