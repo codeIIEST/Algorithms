c29e3a947687a9160eb18e5dd5189be35afda953
Rahul Arulkumaran <rahuldravid313@gmail.com>
2017-12-25 18:01:47 +0530

    Update catalan_binomial.py

--- Competitive Coding/Math/Catalan_Numbers/catalan_binomial.py
+++ Competitive Coding/Math/Catalan_Numbers/catalan_binomial.py
@@ -1,7 +1,7 @@
 def binCoeff(n, k):	#Finds the binomial coefficient
     if (k > n - k):	#As binCoeff(n,k)=binCoeff(n,n-k)
         k = n - k
-    res = 1		#Initialising Result 
+    res = 1		#Initialising Result
     for i in range(k):
         res = res * (n - i)
         res = res / (i + 1)
@@ -10,6 +10,6 @@ def binCoeff(n, k):	#Finds the binomial coefficient
 def catalan(n):		#Function that finds catalan numbers
     c = binCoeff(2*n, n)	#Finding value of c by calling the binCoeff function
     return c/(n + 1)		#This is the final catalan number
- 
+
 for i in range (5):	#finds 1st 5 catalan numbers
     print (catalan(i))	#Prints the Catalan numbers
